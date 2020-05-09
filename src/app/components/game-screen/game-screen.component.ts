9b13736c8a13df8e5685282fdf1c0ef3fbe7ba94
RomanMayer7 <romanmayerzon@gmail.com>
2020-05-09 12:34:41 +0300

    Adding Some Logic for Initializing Game Field

--- src/app/components/game-screen/game-screen.component.ts
+++ src/app/components/game-screen/game-screen.component.ts
@@ -23,7 +23,9 @@ export class GameScreenComponent implements OnInit {
       fieldWidth:number;
       fieldHeight:number;
 
-      gameField: Cell[][] 
+      gameField: Cell[][];
+
+      bobmsAmount:number=30;
 
   constructor() { }
 
@@ -48,6 +50,9 @@ export class GameScreenComponent implements OnInit {
     this.ctx.fillStyle = 'grey';
     this.ctx.fillRect(0, 0, this.fieldWidth, this.fieldHeight);
 
+    this.drawGameFieldLines();
+    this.drawFieldCells();
+
 
 
 
@@ -55,20 +60,182 @@ export class GameScreenComponent implements OnInit {
 
   createGameField()
   {
+    this.gameField=new Array<Array<Cell>>();
+
     for(var n=0;n<this.N;n++)
-    {
+    {    
+      let row:Cell[]  = new Array<Cell>(); 
+      this.gameField.push(row)
+
       for(var m=0;m<this.M;m++)
       {
         this.gameField[n][m]=new Cell();
+        this.gameField[n][m].value=0
+        this.gameField[n][m].visible=true
 
       }
+      
     }
 
   }
 
-  setRandomBombsLocations(){}
 
-  initializeGameField(){}
+  setRandomBombsLocations()
+  {
+    let x=0,y=0
+
+    for(var n=0;n<this.bobmsAmount;n++)
+    {
+
+      while(this.gameField[x][y].value!=0)
+      {
+         x=Math.floor(Math.random() * this.N); 
+         y=Math.floor(Math.random() * this.M);
+      }
+
+      this.gameField[x][y].value=10;
+      
+    }
+
+  }
+  
+
+  initializeGameField()
+  {
+
+    for(var n=0;n<this.N;n++)
+    {
+      for(var m=0;m<this.M;m++)
+      {
+         if(this.gameField[n][m].value==10)
+         {
+          if (( typeof this.gameField[n-1] != 'undefined')&&( typeof this.gameField[n-1][m-1] != 'undefined')&&(this.gameField[n-1][m-1].value!=10))
+          {
+            this.gameField[n-1][m-1].value++;
+          }
+
+          if (( typeof this.gameField[n][m-1]!= 'undefined')&&(this.gameField[n][m-1].value!=10))
+          {
+            this.gameField[n][m-1].value++;
+          }
+
+          if (( typeof this.gameField[n+1] != 'undefined')&&( typeof this.gameField[n+1][m-1] != 'undefined')&&(this.gameField[n+1][m-1].value!=10))
+          {
+            this.gameField[n+1][m-1].value++;
+          }
+
+          //----------------------------------------------------------------------------------
+          if (( typeof this.gameField[n+1] != 'undefined')&&(this.gameField[n+1][m].value!=10))
+          {
+            this.gameField[n+1][m].value++;
+          }
+          if (( typeof this.gameField[n-1] != 'undefined')&&(this.gameField[n-1][m].value!=10))
+          {
+            this.gameField[n-1][m].value++;
+          }
+          //----------------------------------------------------------------------------------
+
+
+          if (( typeof this.gameField[n-1] != 'undefined')&&( typeof this.gameField[n-1][m+1] != 'undefined')&&(this.gameField[n-1][m+1].value!=10))
+          {
+            this.gameField[n-1][m+1].value++;
+          }
+
+          if (( typeof this.gameField[n][m+1]!= 'undefined')&&(this.gameField[n][m+1].value!=10))
+          {
+            this.gameField[n][m+1].value++;
+          }
+
+          if (( typeof this.gameField[n+1] != 'undefined')&&( typeof this.gameField[n+1][m+1] != 'undefined')&&(this.gameField[n+1][m+1].value!=10))
+          {
+            this.gameField[n+1][m+1].value++;
+          }
+
+         }
+
+      }
+    }
+    
+  }
+
+
+  drawLine(x1: number,y1: number,x2: number,y2: number)
+  {
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.strokeStyle = "white";
+    this.ctx.stroke();
+  }
+
+  drawGameFieldLines()
+  {
+    for( var i=0;i<this.fieldWidth;i+=this.Scale)
+    {
+      
+       this.drawLine(i,0,i,this.fieldHeight);
+    }
+
+    for( var j=0;j<this.fieldHeight;j+=this.Scale)
+    {
+      
+       this.drawLine(0,j,this.fieldWidth,j);
+    }
+
+  }
+
+  drawFieldCells()
+  {
+
+    for(var n=0;n<this.N;n++)
+    {
+     console.log("Row Number:"+n)
+      for(var m=0;m<this.M;m++)
+      {
+         let _cell=this.gameField[n][m].value
+         console.log(_cell)
+
+         switch(_cell)
+         {
+ 
+         case 10: { 
+            //Draw Bomb;
+
+            this.ctx.fillStyle = 'red';
+            //console.log("Drawing bomb at:"+m+","+m);
+            this.ctx.fillRect((m)*this.Scale, (n)*this.Scale,this.Scale-2, this.Scale-2) 
+            break;  
+           
+         } 
+         case 1: { 
+            //Draw Cell with Number "1" Label
+            this.ctx.fillStyle = 'blue';
+            this.ctx.font = "20px Arial";
+            this.ctx.fillText(""+this.gameField[n][m].value, (m)*this.Scale+8, (n)*this.Scale+21);
+            break; 
+         } 
+         case 2: { 
+          //Draw Cell with Number "2" Label
+          this.ctx.fillStyle = 'green';
+          this.ctx.font = "20px Arial";
+          this.ctx.fillText(""+this.gameField[n][m].value, (m)*this.Scale+8, (n)*this.Scale+21);
+          break; 
+       }
+       case 3: { 
+        //Draw Cell with Number "3" Label
+        this.ctx.fillStyle = 'red';
+        this.ctx.font = "20px Arial";
+        this.ctx.fillText(""+this.gameField[n][m].value, (m)*this.Scale+8, (n)*this.Scale+21);
+        break; 
+     } 
+
+         }
+          
+      }
+    }
+
+
+  }
+
 
 
 }
